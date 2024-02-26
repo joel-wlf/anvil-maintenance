5f2cdb12bed43acfa80564d6aa01db2aaac3ee17
Joel <72318130+joel-wlf@users.noreply.github.com>
2024-02-26 17:50:58 +0100

    added logn page

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,13 +1,17 @@
 import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Compass } from "lucide-react";
+import { Compass, LogIn } from "lucide-react";
 import { ChangeEvent, useState } from "react";
 import clsx from "clsx";
 
 function LoginForm() {
   const [step, setStep] = useState(1);
-  const [formData, setFormData] = useState<{ url: string }>({ url: "" });
+  const [formData, setFormData] = useState<{
+    url: string;
+    email: string;
+    password: string;
+  }>({ url: "", email: "", password: "" });
 
   function handleChange(e: ChangeEvent<HTMLInputElement>) {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -23,27 +27,62 @@ function LoginForm() {
   }
 
   return (
-    <Card
-      className={clsx(
-        "w-3/4 md:w-1/4 px-4 py-7 flex gap-2 flex-col items-center",
-        { hidden: step != 1 }
-      )}
-    >
-      <Compass size={70} className='mb-2' />
-      <Input
-        type='url'
-        name='url'
-        placeholder='Enter Domain'
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            saveUrl();
-          }
-        }}
-        onChange={handleChange}
-        value={formData.url}
-      />
-      <Button onClick={saveUrl}>Submit</Button>
-    </Card>
+    <>
+      <Card
+        className={clsx(
+          "w-3/4 md:w-1/4 px-4 py-7 flex gap-2 flex-col items-center",
+          { hidden: step != 1 }
+        )}
+      >
+        <Compass size={70} className='mb-2' />
+        <Input
+          type='url'
+          name='url'
+          placeholder='Enter Domain'
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              saveUrl();
+            }
+          }}
+          onChange={handleChange}
+          value={formData.url}
+        />
+        <Button onClick={saveUrl}>Submit</Button>
+      </Card>
+      <Card
+        className={clsx(
+          "w-3/4 md:w-1/4 px-4 py-7 flex gap-2 flex-col items-center",
+          { hidden: step != 2 }
+        )}
+      >
+        <LogIn size={70} className='mb-2' />
+        <Input
+          type='email'
+          name='url'
+          placeholder='Email'
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              saveUrl();
+            }
+          }}
+          onChange={handleChange}
+          value={formData.url}
+        />
+        <Input
+          type='password'
+          name='password'
+          placeholder='Password'
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              saveUrl();
+            }
+          }}
+          onChange={handleChange}
+          value={formData.url}
+        />
+        <Button onClick={saveUrl}>Submit</Button>
+      </Card>
+    </>
   );
 }
 
