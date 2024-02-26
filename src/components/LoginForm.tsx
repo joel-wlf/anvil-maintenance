898c402b21662d9ce7667eca2c03593ffafd2884
Joel <72318130+joel-wlf@users.noreply.github.com>
2024-02-26 17:36:21 +0100

    build(deps): update package-lock.json, LoginForm.tsx and tsconfig.json

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -2,22 +2,34 @@ import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Compass } from "lucide-react";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
+import clsx from "clsx";
 
 function LoginForm() {
-  const [formData, setFormData] = useState({
-    url: "",
-  });
+  const [step, setStep] = useState(1);
+  const [formData, setFormData] = useState<{ url: string }>({ url: "" });
 
-  function handleChange(e: React.FormEvent<HTMLInputElement>) {
-    
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    console.log(formData);
+  }
+
+  function saveUrl() {
+    setStep(2)
+    localStorage.setItem("backendUrl", formData.url);
   }
 
   return (
-    <Card className='w-3/4 md:w-1/4 px-4 py-7 flex gap-2 flex-col items-center'>
+    <Card className={clsx('w-3/4 md:w-1/4 px-4 py-7 flex gap-2 flex-col items-center', {'hidden': step != 1})}>
       <Compass size={70} className='mb-2' />
-      <Input type='url' placeholder='Enter Domain' />
-      <Button>Submit</Button>
+      <Input
+        type='url'
+        name='url'
+        placeholder='Enter Domain'
+        onChange={handleChange}
+        value={formData.url}
+      />
+      <Button onClick={saveUrl}>Submit</Button>
     </Card>
   );
 }
