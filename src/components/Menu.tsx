e24879190c1dbf4a4336858904fea2552c888e37
Joel <72318130+joel-wlf@users.noreply.github.com>
2024-02-27 21:08:23 +0100

    test(menu): test whether user is admin

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,14 +1,21 @@
 import { FunctionComponent } from "react";
+import pb from "@/lib/pocketbase";
 
 interface Props {
   children: React.ReactNode;
 }
 
 const Menu: FunctionComponent<Props> = ({ children }) => {
+  if (pb.authStore.model?.role == "admin") {
+    console.log("admin");
+  }
+
   return (
     <>
-      <h1>Duuu</h1>
-      {children}
+      <div className='hidden md:flex h-lvh w-lvw'>
+        <div className='w-1/5 h-full'></div>
+        <div className='w-4/5 h-full'>{children}</div>
+      </div>
     </>
   );
 };
