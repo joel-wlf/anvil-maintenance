5b5488211d16f09fce97b6d7feb04f51f426a526
Joel <72318130+joel-wlf@users.noreply.github.com>
2024-02-28 17:56:30 +0100

    style(menu): top section layout adjustmends

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,7 +1,9 @@
-import { FunctionComponent } from "react";
 import pb from "@/lib/pocketbase";
+import { Anvil, Compass, LogOut, Settings } from "lucide-react";
+import { FunctionComponent } from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "./ui/button";
+import { Card } from "./ui/card";
 
 interface Props {
   children: React.ReactNode;
@@ -26,10 +28,27 @@ const Menu: FunctionComponent<Props> = ({ children }) => {
   return (
     <>
       <div className='hidden md:flex h-lvh w-lvw'>
-        <div className='w-1/5 h-full border-r border-gray-500'>
-          <h2 className='font-bold text-50'>{pb.authStore.model?.name}</h2>
-          <code>{backendUrl()}</code>
-          <Button onClick={logout}>Log Out</Button>
+        <div className='w-1/5 h-full border-r border-[#222] p-3 flex flex-col gap-3'>
+          <div className='flex justify-start items-center gap-3 p-3 pb-1'>
+            <Anvil size={64} />
+            <h2 className='font-bold text-3xl'>{pb.authStore.model?.name}</h2>
+          </div>
+          <Card className='flex justify-center items-center p-2'>
+            <Compass className='mr-2' size='1.3em' />
+            <code className='text-ellipsis text-sm underline underline-offset-2'>
+              {backendUrl()}
+            </code>
+          </Card>
+          <div className='flex w-full gap-2'>
+            <Button className='w-full' variant='outline' onClick={logout}>
+              <Settings className='mr-2' size='1.3em' />
+              Settings
+            </Button>
+            <Button className='w-full' variant='outline' onClick={logout}>
+              <LogOut className='mr-2' size='1.3em' />
+              Log Out
+            </Button>
+          </div>
         </div>
         <div className='w-4/5 h-full'>{children}</div>
       </div>
