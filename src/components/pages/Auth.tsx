6560b6bf15af284814ed87feb8882d21979e581a
Joel <72318130+joel-wlf@users.noreply.github.com>
2024-02-29 21:23:40 +0100

    feat(login): redirect authenticated user automattically to dashboard

--- src/components/pages/Auth.tsx
+++ src/components/pages/Auth.tsx
@@ -1,9 +1,19 @@
 import LoginForm from "@/components/LoginForm";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DomainForm from "@/components/DomainForm";
+import { useNavigate } from "react-router-dom";
+import { pb } from "@/lib/pocketbase";
 
 function Auth() {
   const [step, setStep] = useState(1);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (pb.authStore.isValid) {
+      navigate("/dashboard");
+    }
+  });
 
   return (
     <div className='h-lvh w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center'>
