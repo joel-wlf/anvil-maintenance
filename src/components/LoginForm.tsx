235d57555a2c0c64045481aa57ea4d645a2a41f4
Joel <72318130+joel-wlf@users.noreply.github.com>
2024-02-29 19:09:35 +0100

    refactor(login): move DomainForm into own component

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -3,40 +3,29 @@ import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import pb from "@/lib/pocketbase.ts";
 import clsx from "clsx";
-import { Compass, LogIn } from "lucide-react";
-import { ChangeEvent, useEffect, useState } from "react";
+import { LogIn } from "lucide-react";
+import { ChangeEvent, FunctionComponent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useToast } from "./ui/use-toast";
 
-function LoginForm() {
-  const [step, setStep] = useState(1);
+interface Props {
+  setStep: (step: number) => void;
+}
 
+const LoginForm: FunctionComponent<Props> = ({ setStep }) => {
   const [formData, setFormData] = useState<{
-    url: string;
     email: string;
     password: string;
-  }>({ url: "", email: "", password: "" });
+  }>({ email: "", password: "" });
 
   const [loading, setLoading] = useState(false);
 
-  const navigate = useNavigate();
-
   const { toast } = useToast();
+
+  const navigate = useNavigate();
 
   function handleChange(e: ChangeEvent<HTMLInputElement>) {
     setFormData({ ...formData, [e.target.name]: e.target.value });
-  }
-
-  function saveUrl() {
-    if (!formData.url) {
-      toast({
-        description: "Please enter your Domain",
-        variant: "destructive",
-      });
-      return;
-    }
-    localStorage.setItem("backendUrl", formData.url);
-    setStep(2);
   }
 
   async function login() {
@@ -57,85 +46,50 @@ function LoginForm() {
     }
   }
 
-  useEffect(() => {
-    if (pb.authStore.isValid) {
-      navigate("/dashboard");
-    }
-  }, []);
-
   return (
-    <>
-      <Card
-        className={clsx(
-          "z-10 w-4/5 md:w-2/6 lg:w-1/4 px-4 py-7 flex gap-2 flex-col items-center",
-          { hidden: step != 1 }
-        )}
-      >
-        <Compass size={70} className='mb-2' />
-        <Input
-          type='url'
-          name='url'
-          placeholder='Enter Domain'
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              saveUrl();
-            }
-          }}
-          onChange={handleChange}
-          value={formData.url}
-        />
-        <div className='flex w-full justify-between pt-3'>
-          <Button variant='ghost' disabled={true}>
-            Back
-          </Button>
-          <Button onClick={saveUrl}>Submit</Button>
-        </div>
-      </Card>
-      <Card
-        className={clsx(
-          "z-10 w-4/5 md:w-2/6 lg:w-1/4 px-4 py-7 flex gap-2 flex-col items-center",
-          { hidden: step != 2 }
-        )}
-      >
-        <LogIn size={70} className='mb-2' />
-        <code className='w-full text-center font-semibold text-sm underline underline-offset-2'>
-          {formData.url}
-        </code>
-        <Input
-          type='email'
-          name='email'
-          placeholder='Email'
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              login();
-            }
-          }}
-          onChange={handleChange}
-          value={formData.email}
-        />
-        <Input
-          type='password'
-          name='password'
-          placeholder='Password'
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              login();
-            }
-          }}
-          onChange={handleChange}
-          value={formData.password}
-        />
-        <div className='flex w-full justify-between pt-3'>
-          <Button variant='ghost' onClick={() => setStep(1)} disabled={loading}>
-            Back
-          </Button>
-          <Button onClick={login} disabled={loading}>
-            {loading ? "Loading..." : "Submit"}
-          </Button>
-        </div>
-      </Card>
-    </>
+    <Card
+      className={clsx(
+        "z-10 w-4/5 md:w-2/6 lg:w-1/4 px-4 py-7 flex gap-2 flex-col items-center"
+      )}
+    >
+      <LogIn size={70} className='mb-2' />
+      <code className='w-full text-center font-semibold text-sm underline underline-offset-2'>
+        {localStorage.getItem('backendUrl')}
+      </code>
+      <Input
+        type='email'
+        name='email'
+        placeholder='Email'
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            login();
+          }
+        }}
+        onChange={handleChange}
+        value={formData.email}
+      />
+      <Input
+        type='password'
+        name='password'
+        placeholder='Password'
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            login();
+          }
+        }}
+        onChange={handleChange}
+        value={formData.password}
+      />
+      <div className='flex w-full justify-between pt-3'>
+        <Button variant='ghost' onClick={() => setStep(1)} disabled={loading}>
+          Back
+        </Button>
+        <Button onClick={login} disabled={loading}>
+          {loading ? "Loading..." : "Submit"}
+        </Button>
+      </div>
+    </Card>
   );
-}
+};
 
 export default LoginForm;
--- src/components/pages/Auth.tsx
+++ src/components/pages/Auth.tsx
@@ -1,10 +1,18 @@
 import LoginForm from "@/components/LoginForm";
+import { useState } from "react";
+import DomainForm from "@/components/DomainForm";
 
 function Auth() {
+  const [step, setStep] = useState(1);
+
   return (
     <div className='h-lvh w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center'>
       <div className='absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]'></div>
-      <LoginForm />
+      {step == 1 ? (
+        <DomainForm setStep={setStep} />
+      ) : (
+        <LoginForm setStep={setStep} />
+      )}
     </div>
   );
 }
