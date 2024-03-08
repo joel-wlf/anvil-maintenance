674003477c582f5ea89cbaac1a2a0f288e022f8c
Joel <72318130+joel-wlf@users.noreply.github.com>
2024-03-08 21:51:16 +0100

    feat(admin): display user Data

--- src/components/pages/Admin.tsx
+++ src/components/pages/Admin.tsx
@@ -1,16 +1,38 @@
 import { pb } from "@/lib/pocketbase";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Plus } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 function Admin() {
   const navigate = useNavigate();
+
+  const [users, setUsers] = useState<any | null>(null);
+
+  async function fetchUsers() {
+    const request = await pb
+      .collection("users")
+      .getFullList({ requestKey: null });
+    setUsers(request);
+  }
 
   useEffect(() => {
     if (!pb.authStore.isValid) {
       navigate("/login");
     }
+    fetchUsers();
   }, []);
-  return <>Admin</>;
+
+  return (
+    <div>
+      <p className='text-2xl md:text-3xl font-semibold px-3 py-2'>Admin</p>
+      <Button>
+        <Plus className='mr-2' size='1.3em' />
+        Create User
+      </Button>
+      {JSON.stringify(users)}
+    </div>
+  );
 }
 
 export default Admin;
