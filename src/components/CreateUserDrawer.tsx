c39408f71171e21fff5db9e1f0855f8d61f520ba
Joel <72318130+joel-wlf@users.noreply.github.com>
2024-03-09 19:51:44 +0100

    feat(createUser): added form elements

--- src/components/CreateUserDrawer.tsx
+++ src/components/CreateUserDrawer.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from "react";
+import { Button } from "@/components/ui/button";
 import {
   Drawer,
   DrawerContent,
@@ -7,26 +7,95 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { ChangeEvent, FunctionComponent, useState } from "react";
 
 interface CreateUserDrawerProps {
   open: boolean;
   setOpen: (open: boolean) => void;
 }
- 
-const CreateUserDrawer: FunctionComponent<CreateUserDrawerProps> = ({open, setOpen}) => {
-    return (
-      <Drawer open={open} onOpenChange={setOpen}>
-        <DrawerTrigger asChild></DrawerTrigger>
-        <DrawerContent>
-          <DrawerHeader className='text-left'>
-            <DrawerTitle>Create User</DrawerTitle>
-            <DrawerDescription>
-              Create a new user for your organisation.
-            </DrawerDescription>
-          </DrawerHeader>
-        </DrawerContent>
-      </Drawer>
-    );
-}
- 
+
+const CreateUserDrawer: FunctionComponent<CreateUserDrawerProps> = ({
+  open,
+  setOpen,
+}) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    role: "",
+    password: "",
+    passwordConfirm: "",
+  });
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    setFormData((prevState) => {
+      return { ...prevState, [e.target.name]: e.target.value };
+    });
+    console.log(formData);
+  }
+
+  return (
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild></DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader className='text-left'>
+          <DrawerTitle>Create User</DrawerTitle>
+          <DrawerDescription>
+            Create a new user for your organisation.
+          </DrawerDescription>
+        </DrawerHeader>
+        <div className='flex flex-col gap-2 px-4 pb-5'>
+          <Input
+            type='text'
+            name='name'
+            value={formData.name}
+            placeholder='Name'
+            onChange={handleChange}
+          />
+          <Input
+            type='email'
+            name='email'
+            value={formData.email}
+            placeholder='Email'
+            onChange={handleChange}
+          />
+          <Select>
+            <SelectTrigger>
+              <SelectValue placeholder='Select role' />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value='user'>User</SelectItem>
+              <SelectItem value='admin'>Admin</SelectItem>
+            </SelectContent>
+          </Select>
+          <div className='flex items-center justify-between gap-2'>
+            <Input
+              type='password'
+              name='password'
+              value={formData.password}
+              placeholder='Password (min. 8)'
+              onChange={handleChange}
+            />
+            <Input
+              type='password'
+              name='passwordConfirm'
+              value={formData.passwordConfirm}
+              placeholder='Confirm Password'
+              onChange={handleChange}
+            />
+          </div>
+          <Button>Create User</Button>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
 export default CreateUserDrawer;
