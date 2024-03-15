bd47cc2094beb9822f004d111646ffde82ec36d8
Joel <72318130+joel-wlf@users.noreply.github.com>
2024-03-15 20:48:02 +0100

    feat(facility): added DeviceItem

---
+++ src/components/DeviceItem.tsx
@@ -0,0 +1,84 @@
+import { FunctionComponent, useState } from "react";
+import { Card } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { MapPin, Activity, Clock, ChevronRight, Locate } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import clsx from "clsx";
+
+interface DeviceItemProps {
+  id: string;
+  name: string;
+  description: string;
+  functional: boolean;
+  location: any;
+  created: string;
+}
+
+const DeviceItem: FunctionComponent<DeviceItemProps> = ({
+  id,
+  name,
+  description,
+  functional,
+  location,
+  created,
+}) => {
+  const [collapsed, setCollapsed] = useState(true);
+
+  return (
+    <Card className='p-3'>
+      <div
+        className='flex items-center justify-between w-full'
+        onClick={() => setCollapsed((prevState) => !prevState)}
+      >
+        <p className='text-[#adadad] max-w-[80%]'>
+          <span className='font-medium text-white'>{name}</span> - {description}
+        </p>
+        <ChevronRight
+          className={clsx("transition-all mx-2", { "rotate-90": !collapsed })}
+          color='#adadad'
+          size={18}
+        />
+      </div>
+      <Separator className='my-1 mt-2' />
+      <div className='flex text-[#adadad] items-center justify-start w-auto gap-2'>
+        <MapPin size='1.3em' />
+        <Button className='p-0 text-[#adadad]' variant='link'>
+          {location.name}
+        </Button>
+      </div>
+      {!collapsed && (
+        <>
+          <Separator className='my-1' />
+          <div className='flex text-[#adadad] items-center justify-start w-auto gap-2'>
+            <Activity size='1.3em' />
+            <Button className='p-0 text-[#adadad]' variant='link'>
+              {functional ? "Functional" : "Not Functional"}
+            </Button>
+          </div>
+          <Separator className='my-1' />
+          <div className='flex text-[#adadad] items-center justify-start w-auto gap-2'>
+            <Clock size='1.3em' />
+            <Button className='p-0 text-[#adadad]' variant='link'>
+              {created.split(" ")[0]}
+            </Button>
+          </div>
+          <Separator className='my-1 mb-2' />
+          <Button variant='outline' className='w-full mb-2'>
+            <Locate size='1.3em' className='mr-2' />
+            Create Task with Device
+          </Button>
+          <div className='flex gap-2'>
+            <Button variant='destructive' className='w-full'>
+              {collapsed ? "Deleting..." : "Delete"}
+            </Button>
+            <Button variant='outline' className='w-full'>
+              Edit
+            </Button>
+          </div>
+        </>
+      )}
+    </Card>
+  );
+};
+
+export default DeviceItem;
