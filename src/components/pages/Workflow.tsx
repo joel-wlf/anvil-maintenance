3d53fd66335c4b60822c348b749b32e960538c9a
Joel <72318130+joel-wlf@users.noreply.github.com>
2024-05-02 21:52:05 +0200

    feat(workflow): fetch task and save it in state

--- src/components/pages/Workflow.tsx
+++ src/components/pages/Workflow.tsx
@@ -1,9 +1,50 @@
-import { useParams } from "react-router-dom";
+import { pb } from "@/lib/pocketbase";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { Subtask, Task } from "@/components/pages/CreateTask";
+import { RecordModel } from "pocketbase";
 
 function Workflow() {
   const { taskId } = useParams();
 
-  return <>{taskId}</>;
+  const navigate = useNavigate();
+
+  const [subtasks, setSubtasks] = useState<Subtask[] | RecordModel[] | null>(
+    []
+  );
+
+  const [task, setTask] = useState<Task | RecordModel>({
+    created_by: "",
+    title: "",
+    status: "",
+    due: "",
+    device: "",
+    assignees: [],
+    subtasks: [],
+    notes: "",
+  });
+
+  async function fetchTask() {
+    const request = await pb
+      .collection("tasks")
+      .getOne(taskId!, { expand: "subtasks", requestKey: null });
+    setTask(request);
+    setSubtasks(request.expand?.subtasks || []);
+  }
+
+  useEffect(() => {
+    if (!pb.authStore.isValid) {
+      navigate("/login");
+    }
+
+    fetchTask();
+  }, []);
+
+  return (
+    <div>
+        test
+    </div>
+  )
 }
 
 export default Workflow;
