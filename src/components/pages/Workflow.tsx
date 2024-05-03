460712abb6c5c97186d9dc5c93706bf7f62e96d5
Joel <72318130+joel-wlf@users.noreply.github.com>
2024-05-03 18:20:59 +0200

    feat(workflow): added second step

--- src/components/pages/Workflow.tsx
+++ src/components/pages/Workflow.tsx
@@ -1,4 +1,4 @@
-import { Subtask, Task } from "@/components/pages/CreateTask";
+import { Task } from "@/components/pages/CreateTask";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { pb } from "@/lib/pocketbase";
@@ -13,6 +13,7 @@ import {
 import { RecordModel } from "pocketbase";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import Subtask from "@/components/Subtask";
 
 function Workflow() {
   const { taskId } = useParams();
@@ -23,7 +24,7 @@ function Workflow() {
 
   const [loading, setLoading] = useState(false);
 
-  const [subtasks, setSubtasks] = useState<Subtask[] | RecordModel[] | null>(
+  const [subtasks, setSubtasks] = useState<any | null>(
     []
   );
 
@@ -78,31 +79,55 @@ function Workflow() {
   return (
     <div className='h-[80vh] w-full'>
       <p className='text-2xl md:text-3xl font-semibold md:pt-2'>{task.title}</p>
-      <div className='flex gap-2 w-5/6 h-auto'>
-        <Card className='flex flex-col p-3 w-3/5 h-full'>
-          <p className='flex'>
-            <Plug className='mr-1' size='1.3em' />
-            {task.expand?.device.name}
-          </p>
-          <p className='flex'>
-            <MapPin className='mr-1' size='1.3em' />
-            {task.expand?.device.expand.location.name}
-          </p>
-          <p className='flex'>
-            <Clock className='mr-1' size='1.3em' />
-            {task.due.split(" ")[0]}
-          </p>
-        </Card>
-        <Card className='flex flex-col gap-2 items-center justify-center w-2/5 h-1/1'>
-          {icon()}
-          {status()}
-        </Card>
-      </div>
+      {step == 1 && (
+        <div className='flex gap-2 w-5/6 h-auto'>
+          <Card className='flex flex-col p-3 w-3/5 h-full'>
+            <p className='flex'>
+              <Plug className='mr-1' size='1.3em' />
+              {task.expand?.device.name}
+            </p>
+            <p className='flex'>
+              <MapPin className='mr-1' size='1.3em' />
+              {task.expand?.device.expand.location.name}
+            </p>
+            <p className='flex'>
+              <Clock className='mr-1' size='1.3em' />
+              {task.due.split(" ")[0]}
+            </p>
+          </Card>
+          <Card className='flex flex-col gap-2 items-center justify-center w-2/5 h-1/1'>
+            {icon()}
+            {status()}
+          </Card>
+        </div>
+      )}
+      {step == 2 && (
+        <div>
+          {subtasks &&
+            subtasks!.map((subtask: any) => {
+              return (
+                <Subtask
+                  key={subtask.id}
+                  id={subtask.id}
+                  name={subtask.name}
+                  done={subtask.done}
+                  setTask={setTask}
+                  setSubtasks={setSubtasks}
+                  deleteDisabled
+                />
+              );
+            })}
+        </div>
+      )}
       <div className='flex w-full justify-between pt-3'>
-        <Button disabled={step == 1} variant='ghost' onClick={() => setStep(prevState => prevState -= 1)}>
+        <Button
+          disabled={step == 1}
+          variant='ghost'
+          onClick={() => setStep((prevState) => (prevState -= 1))}
+        >
           Back
         </Button>
-        <Button onClick={() => setStep(prevState => prevState += 1)}>
+        <Button onClick={() => setStep((prevState) => (prevState += 1))}>
           Next Step
           <ArrowRight size='1.3em' className='ml-1' />
         </Button>
