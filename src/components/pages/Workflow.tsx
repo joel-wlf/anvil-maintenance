47ae31159410062892b896e497b7499377c55313
Joel <72318130+joel-wlf@users.noreply.github.com>
2024-05-05 14:40:31 +0200

    feat(workflow): added reschedule ui

--- src/components/pages/Workflow.tsx
+++ src/components/pages/Workflow.tsx
@@ -3,30 +3,47 @@ import { Task } from "@/components/pages/CreateTask";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import {
-    Dialog,
-    DialogClose,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
 } from "@/components/ui/dialog";
 import { pb } from "@/lib/pocketbase";
-import { CheckCircle2, Clock, Construction, MapPin, Plug } from "lucide-react";
+import {
+  CheckCircle2,
+  Clock,
+  Construction,
+  Download,
+  FileBadge,
+  MapPin,
+  Plug,
+  Repeat,
+} from "lucide-react";
 import { RecordModel } from "pocketbase";
-import { useEffect, useRef, useState } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import SignaturePad from "react-signature-canvas";
+import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { useToast } from "@/components/ui/use-toast";
 
 function Workflow() {
   const { taskId } = useParams();
 
+  const { toast } = useToast();
+
   const navigate = useNavigate();
 
-  const [loading, setLoading] = useState(false);
+//   const [loading, setLoading] = useState(false);
 
   const [imageURL, setImageURL] = useState(null);
+
+  const [rescheduled, setRescheduled] = useState(false);
+
+  const [amount, setAmount] = useState("");
 
   const sigCanvas = useRef<any>({});
 
@@ -50,6 +67,11 @@ function Workflow() {
     });
     setTask(request);
     setSubtasks(request.expand?.subtasks || []);
+  }
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    setRescheduled(false)
+    setAmount(e.target.value)
   }
 
   const icon = () => {
@@ -77,7 +99,13 @@ function Workflow() {
   }
 
   async function generatePdf() {
-    saveSig()
+    saveSig();
+  }
+
+  async function reschedule() {
+    setRescheduled(true)
+    toast({ title: "Successfully rescheduled task." });
+
   }
 
   useEffect(() => {
@@ -111,7 +139,7 @@ function Workflow() {
           {status()}
         </Card>
       </div>
-      <div>
+      <Card>
         {subtasks &&
           subtasks!.map((subtask: any) => {
             return (
@@ -126,26 +154,52 @@ function Workflow() {
               />
             );
           })}
-      </div>
-      <img src={imageURL!} />
+      </Card>
+      <Card>
+        <Input type='number' placeholder='Amount' onChange={handleChange}/>
+        <Select defaultValue='months'>
+          <SelectTrigger>
+            <SelectValue placeholder='Months' />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value='days'>Days</SelectItem>
+            <SelectItem value='weeks'>Weeks</SelectItem>
+            <SelectItem value='months'>Months</SelectItem>
+            <SelectItem value='years'>Years</SelectItem>
+          </SelectContent>
+        </Select>
+        <Button onClick={reschedule} disabled={rescheduled || !amount}>
+          Reschedule
+          <Repeat size='1.3em' className='ml-2' />
+        </Button>
+      </Card>
       <Dialog>
         <DialogTrigger asChild>
-          <Button>Document and Save</Button>
+          <Button>
+            Document + Save
+            <FileBadge size='1.3em' className='ml-2' />
+          </Button>
         </DialogTrigger>
-        <DialogContent className="w-[95%] rounded-lg">
-            <DialogHeader className="items-start">
-              <DialogTitle>Sign here</DialogTitle>
-              <DialogDescription>Sign the document to confirm your work.</DialogDescription>
-            </DialogHeader>
-            <SignaturePad
-              ref={sigCanvas}
-              penColor='white'
-              canvasProps={{
-                className: "border-[1px] border-[#333] rounded-lg w-full h-[20vh]",
-              }}
-            />
+        <DialogContent className='w-[95%] rounded-lg'>
+          <DialogHeader className='items-start'>
+            <DialogTitle>Sign here</DialogTitle>
+            <DialogDescription>
+              Sign the document to confirm your work.
+            </DialogDescription>
+          </DialogHeader>
+          <SignaturePad
+            ref={sigCanvas}
+            penColor='white'
+            canvasProps={{
+              className:
+                "border-[1px] border-[#333] rounded-lg w-full h-[20vh]",
+            }}
+          />
           <DialogFooter>
-            <Button className="w-full" onClick={generatePdf}>Generate Report + Mark as Done</Button>
+            <Button className='w-full' onClick={generatePdf}>
+              Generate Report + Mark as Done
+              <Download size='1.3em' className='ml-2' />
+            </Button>
           </DialogFooter>
         </DialogContent>
       </Dialog>
