7d28bd0e38f941bb40fed8d9c03a729bcd733c17
Joel Wolf <72318130+joel-wlf@users.noreply.github.com>
2024-05-11 17:32:05 +0200

    feat(workflow): added loading state (today is my birthday)

--- src/components/pages/Workflow.tsx
+++ src/components/pages/Workflow.tsx
@@ -27,9 +27,16 @@ import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import SignaturePad from "react-signature-canvas";
 import { Input } from "@/components/ui/input";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { useToast } from "@/components/ui/use-toast";
 import { addDays, addMonths, addWeeks, addYears } from "date-fns";
+import { Skeleton } from "@/components/ui/skeleton";
 
 function Workflow() {
   const { taskId } = useParams();
@@ -38,7 +45,7 @@ function Workflow() {
 
   const navigate = useNavigate();
 
-//   const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const [imageURL, setImageURL] = useState(null);
 
@@ -46,7 +53,7 @@ function Workflow() {
 
   const [amount, setAmount] = useState<any>();
 
-  const [amountType, setAmountType] = useState("months")
+  const [amountType, setAmountType] = useState("months");
 
   const sigCanvas = useRef<any>({});
 
@@ -69,12 +76,13 @@ function Workflow() {
       requestKey: null,
     });
     setTask(request);
+    setLoading(false);
     setSubtasks(request.expand?.subtasks || []);
   }
 
   function handleAmountChange(e: ChangeEvent<HTMLInputElement>) {
-    setRescheduled(false)
-    setAmount(e.target.value)
+    setRescheduled(false);
+    setAmount(e.target.value);
   }
 
   const icon = () => {
@@ -97,32 +105,58 @@ function Workflow() {
     }
   };
 
+  const Subtasks = () => {
+    if (subtasks.length != 0) {
+      return (
+      subtasks!.map((subtask: any) => {
+        return (
+          <Subtask
+            key={subtask.id}
+            id={subtask.id}
+            name={subtask.name}
+            done={subtask.done}
+            setTask={setTask}
+            setSubtasks={setSubtasks}
+            deleteDisabled
+          />
+        );
+      }))
+    } else {
+      return <div className='text-center text-[#adadad]'>No subtasks here.</div>
+    }
+  }
+
   function saveSig() {
     setImageURL(sigCanvas.current.getTrimmedCanvas().toDataURL("image/png"));
   }
 
   async function generatePdf() {
     saveSig();
-    console.log(imageURL)
+    console.log(imageURL);
   }
 
   async function reschedule() {
-    setRescheduled(true)
-    const today = new Date()
+    setRescheduled(true);
+    const today = new Date();
     const newDate = () => {
-        if (amountType == "days") {
-            return addDays(today, +amount)
-        } else if (amountType == "weeks") {
-          return addWeeks(today, +amount)
-        } else if (amountType == "months") {
-          return addMonths(today, +amount)
-        } else if (amountType == "years") {
-          return addYears(today, +amount)
-        }
-    }
-    const newTask = {...task, id: "", created_by: pb.authStore.model?.id, due: newDate()}
+      if (amountType == "days") {
+        return addDays(today, +amount);
+      } else if (amountType == "weeks") {
+        return addWeeks(today, +amount);
+      } else if (amountType == "months") {
+        return addMonths(today, +amount);
+      } else if (amountType == "years") {
+        return addYears(today, +amount);
+      }
+    };
+    const newTask = {
+      ...task,
+      id: "",
+      created_by: pb.authStore.model?.id,
+      due: newDate(),
+    };
     try {
-      await pb.collection("tasks").create(newTask)
+      await pb.collection("tasks").create(newTask);
       toast({ title: "Successfully rescheduled task." });
     } catch (err: any) {
       toast({ title: err.message, variant: "destructive" });
@@ -139,77 +173,98 @@ function Workflow() {
 
   return (
     <div className='h-[80vh] w-full'>
-      <p className='text-2xl md:text-3xl font-semibold md:pt-2'>{task.title}</p>
+      {loading ? (
+        <Skeleton className='h-6 w-40 md:pt-2' />
+      ) : (
+        <p className='text-2xl md:text-3xl font-semibold md:pt-2'>
+          {task.title}
+        </p>
+      )}
       <div className='flex flex-col w-full gap-5 py-5'>
         <div className='flex gap-5 w-full h-auto'>
-          <Card className='flex flex-col p-3 w-3/5 h-full'>
-            <p className='flex'>
-              <Plug className='mr-1' size='1.3em' />
-              {task.expand?.device.name}
-            </p>
-            <p className='flex'>
-              <MapPin className='mr-1' size='1.3em' />
-              {task.expand?.device.expand.location.name}
-            </p>
-            <p className='flex'>
-              <Clock className='mr-1' size='1.3em' />
-              {task.due.split(" ")[0]}
-            </p>
-          </Card>
-          <Card className='flex flex-col gap-2 items-center justify-center w-2/5 h-1/1'>
-            {icon()}
-            {status()}
-          </Card>
+          {loading ? (
+            <Card className='flex flex-col p-3 w-3/5 gap-1 h-full'>
+              <Skeleton className='h-4 w-full' />
+              <Skeleton className='h-4 w-full' />
+              <Skeleton className='h-4 w-full' />
+            </Card>
+          ) : (
+            <Card className='flex flex-col p-3 w-3/5 h-full'>
+              <p className='flex'>
+                <Plug className='mr-1' size='1.3em' />
+                {task.expand?.device.name}
+              </p>
+              <p className='flex'>
+                <MapPin className='mr-1' size='1.3em' />
+                {task.expand?.device.expand.location.name}
+              </p>
+              <p className='flex'>
+                <Clock className='mr-1' size='1.3em' />
+                {task.due.split(" ")[0]}
+              </p>
+            </Card>
+          )}
+          {loading ? (
+            <Card className='flex flex-col gap-2 items-center justify-center w-2/5 h-1/1'>
+              <Skeleton className='h-6 w-6' />
+              <Skeleton className='h-4 w-16' />
+            </Card>
+          ) : (
+            <Card className='flex flex-col gap-2 items-center justify-center w-2/5 h-1/1'>
+              {icon()}
+              {status()}
+            </Card>
+          )}
         </div>
-        <Card className='p-2'>
-          <p className='font-semibold text-lg px-2'>Subtasks</p>
-          {subtasks &&
-            subtasks!.map((subtask: any) => {
-              return (
-                <Subtask
-                  key={subtask.id}
-                  id={subtask.id}
-                  name={subtask.name}
-                  done={subtask.done}
-                  setTask={setTask}
-                  setSubtasks={setSubtasks}
-                  deleteDisabled
-                />
-              );
-            })}
-        </Card>
+        {loading ? (
+          <Card className='flex flex-col gap-1 p-2'>
+            <p className='font-semibold text-lg px-2'>Subtasks</p>
+            <Skeleton className='h-4 w-full' />
+            <Skeleton className='h-4 w-full' />
+            <Skeleton className='h-4 w-full' />
+          </Card>
+        ) : (
+          <Card className='p-2'>
+            <p className='font-semibold text-lg px-2'>Subtasks</p>
+            <Subtasks />
+          </Card>
+        )}
         <Card className='flex flex-col gap-2 p-2'>
           <p className='font-semibold text-lg px-2'>Reschedule</p>
-          <div className="flex gap-2">
-          <Input
-            type='number'
-            placeholder='Amount'
-            onChange={handleAmountChange}
-          />
-          <Select
-            defaultValue='months'
-            value={amountType}
-            onValueChange={(e) => setAmountType(e)}
-          >
-            <SelectTrigger>
-              <SelectValue placeholder='Months' />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value='days'>Days</SelectItem>
-              <SelectItem value='weeks'>Weeks</SelectItem>
-              <SelectItem value='months'>Months</SelectItem>
-              <SelectItem value='years'>Years</SelectItem>
-            </SelectContent>
-          </Select>
+          <div className='flex gap-2'>
+            <Input
+              type='number'
+              placeholder='Amount'
+              onChange={handleAmountChange}
+            />
+            <Select
+              defaultValue='months'
+              value={amountType}
+              onValueChange={(e) => setAmountType(e)}
+            >
+              <SelectTrigger>
+                <SelectValue placeholder='Months' />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value='days'>Days</SelectItem>
+                <SelectItem value='weeks'>Weeks</SelectItem>
+                <SelectItem value='months'>Months</SelectItem>
+                <SelectItem value='years'>Years</SelectItem>
+              </SelectContent>
+            </Select>
           </div>
-          <Button onClick={reschedule} variant="outline" disabled={rescheduled || !amount}>
+          <Button
+            onClick={reschedule}
+            variant='outline'
+            disabled={rescheduled || !amount || loading}
+          >
             Reschedule Task
             <Repeat size='1.3em' className='ml-2' />
           </Button>
         </Card>
         <Dialog>
           <DialogTrigger asChild>
-            <Button>
+            <Button disabled={loading}>
               Document + Save
               <FileBadge size='1.3em' className='ml-2' />
             </Button>
