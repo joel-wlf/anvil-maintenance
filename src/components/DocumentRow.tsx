0e0ba49c5ad13e124911dbf363bff267bd17bdb7
Joel Wolf <72318130+joel-wlf@users.noreply.github.com>
2024-06-24 00:38:32 +0200

    feat(documentation): added DocumentRow component

---
+++ src/components/DocumentRow.tsx
@@ -0,0 +1,17 @@
+import { Document } from "@/types.ts"
+import { FunctionComponent } from "react";
+
+interface DocumentRowProps {
+    loading: boolean;
+    document: Document;
+}
+ 
+const DocumentRow: FunctionComponent<DocumentRowProps> = () => {
+    return ( 
+        <>
+
+        </>
+    );
+}
+ 
+export default DocumentRow;
